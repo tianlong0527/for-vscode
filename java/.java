026044d4b55821b4c927340f6f073ac2534113f3
tianlong0527 <f74126270@gs.ncku.edu.tw>
2024-04-04 22:01:11 +0800

    update hw2 and some changes

---
+++ java/.java
@@ -0,0 +1,22 @@
+null    private class alculator;
+    public int add(int a,  int, ) {return 0;}
+    public int subtract(int a,  int, ) {return 0;}
+    public int multiply(int a,  int, ) {return 0;}
+    private Calculator ;
+    public int getResult() {
+        return result;
+    }
+    public int addThreeNumbers(int a,  int, ) {return 0;}
+    public int divide(int a,  int, ) {return 0;}
+    public boolean isZero(int a, ) {return false;}
+    public boolean isNegative(int a, ) {return false;}
+    private User ;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void changeName(String name, ) {;}
+    public void sayHello( , ) {;}
+}
