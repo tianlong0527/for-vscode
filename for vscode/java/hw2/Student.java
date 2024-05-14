cdb7001448a7a056ef0fc7d79b1baad5cfb11984
tianlong0527 <153146774+tianlong0527@users.noreply.github.com>
2024-05-14 15:37:24 +0800

    Delete for vscode directory

--- for vscode/java/hw2/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student {
-    public int studentID;
-    public void study() {;}
-    private String studentName;
-    private Teacher supervisor;
-    public String getStudentName() {
-        return studentName;
-    }
-}
--- for vscode/java/hw2/Teacher.java
+++
@@ -1,6 +0,0 @@
-public class Teacher {
-    public String teacherID;
-    public void teach() {;}
-    public String className;
-    public int[] studentArray;
-}
